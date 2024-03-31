9d5abc7193f4425c98d05296fe772a1c7460883d
tuliosalvatierra <salvacorp@gmail.com>
2024-03-31 17:51:41 -0500

    Refactor Hero component layout

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,21 +4,6 @@ import "../../App.css";
 function Hero() {
   return (
     <div className="container">
-      <div className="row align-items-center mb-20 mb-xl-24">
-        <div className="col-12 col-md-6 col-xl-7 mb-6 mb-lg-0">
-          <h1 className="mb-6 lh-sm">Carolina Skin Centre, Midland, TX.</h1>
-          <a className="btn btn-dark d-block d-md-inline-block" href="#">
-            Book an appointment
-          </a>
-        </div>
-        <div className="col-12 col-md-6 col-xl-5">
-          <p className="mb-0 lh-lg mw-md-sm">
-            Welcome to our official website. We are a team of professionals
-            dedicated to providing the best possible care for your skin. We
-            offer a wide range of services to help you look and feel your best.
-          </p>
-        </div>
-      </div>
       <div>
         <img
           className="w-100 mb-10"
@@ -33,6 +18,21 @@ function Hero() {
             alt=""
           />
           <h5 className="mb-0">06</h5>
+        </div>
+      </div>
+      <div className="row align-items-center mb-20 mb-xl-24">
+        <div className="col-12 col-md-6 col-xl-7 mb-6 mb-lg-0">
+          <h1 className="mb-6 lh-sm">Carolina Skin Centre, Midland, TX.</h1>
+          <a className="btn btn-dark d-block d-md-inline-block" href="#">
+            Book an appointment
+          </a>
+        </div>
+        <div className="col-12 col-md-6 col-xl-5">
+          <p className="mb-0 lh-lg mw-md-sm">
+            We are a team of professionals dedicated to providing the best
+            possible care for your skin. We offer a wide range of services to
+            help you look and feel your best.
+          </p>
         </div>
       </div>
     </div>
