db80e79850aa5f6c2907d8a48df89e098ca6f417
Tulio Salvatierra <salvacorp@gmail.com>
2025-02-15 11:50:26 -0600

    style: Update font-family in App.css; adjust overlay background color and hero section height

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,27 +6,27 @@ function Hero() {
   return (
     <>
       <section className="bg-hero d-flex align-items-center justify-content-center">
-      <div className="overlay">
-        <div className="d-flex flex-column flex-lg-row">
-          <div className="col-12 col-lg-7 col-sm-1 p-2 hero-content">
-            <h1 className="display-4 w-100 heading">
-              Hydrate, renew and balance your skin
-            </h1>
-            <p className="w-100 bg-danger-light m-1 p-4 welcome-text fs-6">
-              Improve the health and appearance of their skin through
-              personalized facial treatments that will balance your skin &
-              delivering visible and long-lasting results, located in the heart
-              of Midland, TX.
-            </p>
-            <a
-              className="btn btn-dark p-4 mt-4 w-100 w-md-auto text-white fs-4 fs-sm-6"
-              href={BOOKING_URL}
-            >
-              Book my consultation now!
-            </a>
-            <HeroReviewCarousel />
+        <div className="overlay">
+          <div className="d-flex flex-column flex-lg-row">
+            <div className="col-12 col-lg-7 col-sm-1 p-2 hero-content">
+              <h1 className="display-4 w-100 heading">
+                Hydrate, renew and balance your skin
+              </h1>
+              <p className="w-100 bg-danger-light m-1 p-4 welcome-text fs-6">
+                Improve the health and appearance of their skin through
+                personalized facial treatments that will balance your skin &
+                delivering visible and long-lasting results, located in the
+                heart of Midland, TX.
+              </p>
+              <a
+                className="btn btn-dark p-4 mt-4 w-100 w-md-auto text-white fs-4 fs-sm-6"
+                href={BOOKING_URL}
+              >
+                Book my consultation now!
+              </a>
+              <HeroReviewCarousel />
+            </div>
           </div>
-        </div>
         </div>
       </section>
     </>
