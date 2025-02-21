55ef2b18dbc4a9f1525e33f27e9e25161ad16486
Tulio Salvatierra <salvacorp@gmail.com>
2025-02-21 12:05:28 -0600

    fix: Update BOOKING_URL constant to use telephone link instead of booking URL

--- src/constants/index.js
+++ src/constants/index.js
@@ -17,7 +17,7 @@ export const INSTAGRAM = "https://www.instagram.com/carolinaskin";
 export const TELEPHONE = "432-638-0046";
 
 export const BOOKING_URL =
-  "https://online-booking.salonbiz.com/olb/!page.widget_start?wbid=c1185t201404101626pu568R8C68u8RJ4Pz98Mr&theme=651&p_mobile=y&p_start=1";
+  "tel:432-638-0046";
 
 export const ADDRESS = "705 W Wadley Ave Suite-23, Midland, TX 79705";
 
