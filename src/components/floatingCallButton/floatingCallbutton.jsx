b082a4489d85659751bcdaf4d0418e2382ebe49e
Tulio Salvatierra <salvacorp@gmail.com>
2025-03-09 20:35:14 -0500

    feat: Adjust floating call button position and enhance button styling for better visibility

--- src/components/floatingCallButton/floatingCallbutton.jsx
+++ src/components/floatingCallButton/floatingCallbutton.jsx
@@ -6,7 +6,7 @@ export default function FloatingCallbutton() {
   return (
     <div className="floating-call-button">
       <a href={TELEPHONE}>
-        <button className="btn btn-dark">
+        <button className="btn btn-dark shadow-lg">
           Call us now!
         </button>
       </a>
