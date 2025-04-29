8c1753a05538a6529ef7461e91e92fc86af6ff34
Tulio Salvatierra <salvacorp@gmail.com>
2025-04-28 21:11:05 -0400

    feat: Integrate useFadeInAnimation hook for enhanced fade-in effects in BrandsSectionLogoClouds1 component

--- src/components/BrandSection/BrandsSectionLogoClouds1.js
+++ src/components/BrandSection/BrandsSectionLogoClouds1.js
@@ -3,13 +3,19 @@ import HYDRA from "../../assets/images/hydar.svg";
 import GLO from "../../assets/images/glo_logo.svg";
 import HYDRAPRODUCT from "./../../assets/images/hf.webp";
 import GLOPRODUCT from "./../../assets/images/glo.webp";
+import { useFadeInAnimation } from "./../../hooks/useFadeInAnimation";
+import {  useRef } from "react";
+
 
 export default function BrandsSectionLogoClouds1() {
+
+  const animated = useRef([]);
+  useFadeInAnimation(animated);
   return (
     <>
       <section className="py-48 bg-white">
         <div className="container">
-          <p className="text-muted mb-6 lh-lg text-center">
+          <p  ref={(el) => (animated.current[0] = el)} className="text-muted mb-6 lh-lg text-center">
             Trusted by brands all over the world
           </p>
           <div className="row mx-sm-n4">
@@ -20,10 +26,12 @@ export default function BrandsSectionLogoClouds1() {
                 src={HYDRAPRODUCT}
                 alt="hydrafacial-product-logo"
                 style={{ height: 526, width: "auto" }}
+                ref={(el) => (animated.current[1] = el)}
               />
               <div
                 className="bg-primary-light d-flex align-items-center justify-content-center px-4"
                 style={{ height: 380 }}
+                ref={(el) => (animated.current[2] = el)}
               >
                 <img
                   className="img-fluid mx-auto"
@@ -38,6 +46,7 @@ export default function BrandsSectionLogoClouds1() {
                 src={GLOPRODUCT}
                 alt="hydrafacial-product-logo"
                 style={{ height: 526, width: "auto" }}
+                ref={(el) => (animated.current[3] = el)}
               />
               <div
                 className="bg-secondary-light d-flex align-items-center justify-content-center px-4"
@@ -47,6 +56,7 @@ export default function BrandsSectionLogoClouds1() {
                   className="img-fluid d-block mx-auto"
                   src={GLO}
                   alt="GLO-LOGO"
+                  ref={(el) => (animated.current[4] = el)}
                 />
               </div>
             </div>
